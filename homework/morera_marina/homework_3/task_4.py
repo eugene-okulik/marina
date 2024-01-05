2a1a3bd1c02c80cf8bc31b0df288fc836417f267
Marina1824 <9mariam7@gmail.com>
2024-01-05 12:20:23 +0200

    Homework_3. Edit
    Исправленное дз

--- homework/morera_marina/homework_3/task_4.py
+++ homework/morera_marina/homework_3/task_4.py
@@ -2,7 +2,7 @@ from math import sqrt
 
 x = int(input("Введите первый катет: "))
 y = int(input("Введите второй катет: "))
-hypotenyza = sqrt((x**2)+(y**2))
-S = (x*hypotenyza)/2 #Либо можно было по формуле 0.5 * x * y
-print("Гипотенуза:",hypotenyza, "\nПлощадь:",S)
 
+hypotenuse = sqrt(x ** 2 + y ** 2)
+S = (x * hypotenuse) / 2  # Либо можно было по формуле 0.5 * x * y
+print("Гипотенуза:", hypotenuse, "\nПлощадь:", S)
