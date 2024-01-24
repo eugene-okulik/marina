5f34ed8207dd40582da6eae8dcde1da95d21dd1f
Marina1824 <9mariam7@gmail.com>
2024-01-24 18:25:14 +0200

    Update task_2
    Исправление задания 2

--- homework/morera_marina/homework_5/task_2.py
+++ homework/morera_marina/homework_5/task_2.py
@@ -2,10 +2,10 @@ a = "результат операции: 42"
 b = "результат операции: 514"
 c = "результат работы программы: 9"
 
-result_a = a.index('42')
-result_b = b.index('514')
-result_c = c.index('9')
-new_a = int((a[result_a:]))
-new_b = int((b[result_b:]))
-new_c = int((c[result_c:]))
+result_a = a.index(':')
+result_b = b.index(':')
+result_c = c.index(':')
+new_a = int(a[result_a+1:])
+new_b = int(b[result_b+1:])
+new_c = int(c[result_c+1:])
 print(new_a + 10, new_b + 10, new_c + 10)
