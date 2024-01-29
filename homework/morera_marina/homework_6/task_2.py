5e16d61deadbbd4575717f4589e3e9586b7f423a
Marina1824 <9mariam7@gmail.com>
2024-01-29 18:33:12 +0200

    Homework_6_edit
    Забываю наводить красоту

--- homework/morera_marina/homework_6/task_2.py
+++ homework/morera_marina/homework_6/task_2.py
@@ -1,9 +1,9 @@
 for i in range(1, 101):
-    if i%3==0 and i%5==0:
+    if i % 3 == 0 and i % 5 == 0:
         print("FuzzBuzz")
-    elif i%3==0:
+    elif i % 3 == 0:
         print("Fuzz")
-    elif i%5==0:
+    elif i % 5 == 0:
         print("Buzz")
     else:
         print(i)
