1801e8927561fc138bcf7ddb7f5fefd1a5cd381d
Marina1824 <9mariam7@gmail.com>
2024-03-03 19:33:37 +0200

    Homework_9
    Task 1 and Task 2

---
+++ homework/morera_marina/homework_9/task_2.py
@@ -0,0 +1,7 @@
+temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32,
+                30, 28, 24, 23]
+
+new_temperatures = list(filter(lambda x: x >= 28, temperatures))
+
+print(f'Самая высокая температура: {max(new_temperatures)}. Самая низкая температура: {min(new_temperatures)}. '
+      f'Средняя температура {sum(new_temperatures) / len(new_temperatures)}')
