5eb4c7b426567fc2e7fb1efcb6d13d2df1fb846b
Marina1824 <9mariam7@gmail.com>
2024-04-28 22:30:48 +0300

    program

---
+++ homework/morera_marina/program/task_1.py
@@ -0,0 +1,22 @@
+import os
+
+# file_path = 'C:/Users/9mari/projects/marina/homework/eugene_okulik/data/logs'
+file_path = input('Введите полный путь к папке, в которой лежат файлы с логами')
+dir_list = os.listdir(file_path)
+print(dir_list)
+text_file = input('Введите текст который нужно найти в файлах')
+# text_file = 'hikari.pool.HikariProxyPreparedStatement.executeQuery(HikariProxyPreparedStatement.java) ~[HikariCP-4.0.3.jar!/:na]'
+
+for file_name in dir_list:
+    file_text = os.path.join(file_path, file_name)
+    with open(file_text, 'r') as file:
+        lines = file.readlines()
+        line_number = 0
+        for line in lines:
+            line_number += 1
+            if text_file in line:
+                print(f'Найдено в файле: {file_name}. Порядковый номер строки файла {line_number}')
+                index = line.find(text_file)
+                start = max(0, index - 5)
+                end = min(len(line), index + len(text_file) + 5)
+                print(line[start:end])
