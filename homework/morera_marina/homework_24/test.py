12f260743fdf74e6ecfb65cbeb92adf5fb7118ef
Marina1824 <9mariam7@gmail.com>
2024-08-11 23:45:40 +0300

    Update test.py

--- homework/morera_marina/homework_24/test.py
+++ homework/morera_marina/homework_24/test.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.alert import Alert
 
+
 @pytest.fixture()
 def driver():
     chrome_driver = webdriver.Chrome()
@@ -17,7 +18,8 @@ def driver():
 def test_shop(driver):
     driver.get("https://www.demoblaze.com/index.html")
     link = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, 'card-img-top')))
+        EC.presence_of_element_located((By.CLASS_NAME, 'card-img-top'))
+    )
     ActionChains(driver).key_down(Keys.CONTROL).click(link).key_up(Keys.CONTROL).perform()
     WebDriverWait(driver, 10).until(lambda d: len(d.window_handles) == 2)
     tabs = driver.window_handles
@@ -34,21 +36,25 @@ def test_shop(driver):
     driver.switch_to.window(tabs[0])
     driver.find_element(By.ID, 'cartur').click()
     cart_item = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="tbodyid"]/tr/td[2]')))
+        EC.presence_of_element_located((By.XPATH, '//*[@id="tbodyid"]/tr/td[2]'))
+    )
     assert cart_item.text == text_item
 
 
 def test_luma(driver):
     driver.get("https://magento.softwaretestingboard.com/gear/bags.html")
     item = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, 'product-image-wrapper')))
+        EC.presence_of_element_located((By.CLASS_NAME, 'product-image-wrapper'))
+    )
     actions = ActionChains(driver)
     actions.move_to_element(item).perform()
     compare = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, 'tocompare')))
+        EC.presence_of_element_located((By.CLASS_NAME, 'tocompare'))
+    )
     actions.click(compare).perform()
     item_text = driver.find_element(By.CLASS_NAME, 'product-item-link').text
     driver.execute_script("window.scrollTo(0, 500)")
     compare_text = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="compare-items"]/li/strong/a')))
+        EC.presence_of_element_located((By.XPATH, '//*[@id="compare-items"]/li/strong/a'))
+    )
     assert item_text == compare_text.text
