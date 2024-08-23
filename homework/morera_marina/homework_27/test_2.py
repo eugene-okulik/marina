04715de56d078b0c031ef5ddc4a212ff92cb7a39
Marina1824 <9mariam7@gmail.com>
2024-08-23 17:49:12 +0300

    test_2_half

---
+++ homework/morera_marina/homework_27/test_2.py
@@ -0,0 +1,13 @@
+from playwright.sync_api import Page, expect, BrowserContext
+
+
+def test_new_page(page: Page, context: BrowserContext):
+    page.on('dialog', lambda dialog: dialog.accept())
+    page.goto('https://www.qa-practice.com/elements/new_tab/button')
+    link = page.get_by_role('link', name='Click')
+    with context.expect_page() as new_page_event:
+        link.click()
+    new_page = new_page_event.value
+    result = new_page.locator('#result-text')
+    expect(result).to_have_text('I am a new page in a new tab')
+
