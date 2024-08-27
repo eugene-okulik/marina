8d181171e6da256e9483854d71f3993cea719482
Marina1824 <9mariam7@gmail.com>
2024-08-27 14:03:09 +0300

    Update test_alert.py

--- homework/morera_marina/homework_27/test_alert.py
+++ homework/morera_marina/homework_27/test_alert.py
@@ -5,6 +5,5 @@ def test_alert(page: Page):
     page.on('dialog', lambda dialog: dialog.accept())
     page.goto('https://www.qa-practice.com/elements/alert/confirm')
     page.get_by_role('link', name='click').click()
-
     result = page.locator('#result-text')
     expect(result).to_have_text('Ok')
