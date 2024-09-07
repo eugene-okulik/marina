a90ab791df956d86937beb049c0de78fc010bd56
Marina1824 <9mariam7@gmail.com>
2024-09-07 14:04:01 +0300

    Done task1

--- homework/morera_marina/homework_28/test.py
+++ homework/morera_marina/homework_28/test.py
@@ -1,29 +1,17 @@
-import re
-from playwright.sync_api import Page, expect, Request, Route
-from time import sleep
+from playwright.sync_api import Page, Route
 import json
 
 
-def test(page: Page):
-
-    def print_request(request: Request):
-        print("request", request.post_data)
-
-    page.on("request", print_request)
-    page.goto("https://www.apple.com/shop/buy-iphone")
-    with page.expect_response(re.compile('s68030986690955')) as response_event:
-        page.locator('text="iPhone 15 Pro &"').click()
-    response = response_event.value
-    print(response)
-
-    sleep(5)
 def test_two(page: Page):
     def handle_route(route: Route):
         response = route.fetch()
         body = response.json()
+
         for item in body.get('body', {}).get('digitalMat', []):
-            if item.get('productName') == 'iPhone 15 Pro':
+            if item.get('productName') == 'iPhone 15 Pro':
                 item['productName'] = 'айфончик'
+                for familyType in item.get('familyTypes', []):
+                    familyType['productName'] = 'айфончик'
         body = json.dumps(body)
         route.fulfill(
             response=response,
@@ -33,5 +21,5 @@ def test_two(page: Page):
     page.route('**/digital-mat?path=library/step0_iphone/digitalmat', handle_route)
     page.goto("https://www.apple.com/shop/buy-iphone")
     page.locator('text="iPhone 15 Pro &"').click()
-    sleep(5)
-
+    page.wait_for_selector('text="айфончик"')
+    assert page.locator('text="айфончик"').count() > 0
