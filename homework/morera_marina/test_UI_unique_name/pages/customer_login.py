b974ca1f3724fba25387b06728c8246c2a82c687
Marina1824 <9mariam7@gmail.com>
2024-09-11 13:49:26 +0300

    test registration

--- homework/morera_marina/test_UI_unique_name/pages/customer_login.py
+++ homework/morera_marina/test_UI_unique_name/pages/customer_login.py
@@ -1,5 +1,5 @@
 from playwright.sync_api import Page
-from playwright.sync_api import Expect
+
 
 class Customerlogin:
     def __init__(self, page: Page):
@@ -9,36 +9,29 @@ class Customerlogin:
         self.page.goto('https://magento.softwaretestingboard.com/customer/account/create/')
 
     def registration_form(self, first, last, email, password, confirm_pas):
-        first_name = self.page.locator('firstname')
-        last_name = self.page.locator('lastname')
-        email_field = self.page.locator('email_address')
-        password_field = self.page.locator('password')
-        confirm_field = self.page.locator('password-confirmation')
-        submit = self.page.locator('submit')
+        first_name = self.page.locator('#firstname')
+        last_name = self.page.locator('#lastname')
+        email_field = self.page.locator('#email_address')
+        password_field = self.page.locator('#password')
+        confirm_field = self.page.locator('#password-confirmation')
+        submit = self.page.get_by_role('button', name='Create an Account')
         first_name.fill(first)
         last_name.fill(last)
         email_field.fill(email)
         password_field.fill(password)
         confirm_field.fill(confirm_pas)
+
         submit.click()
 
     def check_success_text(self, expected_text):
-        registering = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="maincontent"]/div[1]/div[2]/div/div/div'))
-
-        )
-        assert registering.text == expected_text
+        registering = self.page.locator('div.message-success')
+        assert registering.inner_text() == expected_text
 
     def check_text(self, expected_text):
-        registering = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="maincontent"]/div[2]'))
-
-        )
-        assert registering.text == expected_text
+        registering = self.page.locator('div.message-error')
+        assert registering.inner_text() == expected_text
 
     def exist_email_message_is(self, expected_text):
-        email = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="email_address-error"]'))
-        )
-        assert email.text == expected_text
+        email = self.page.locator('#email_address-error')
+        assert email.inner_text() == expected_text
 
