4be92d9baf3cd4d60d022b95dbad11267f29131a
Marina1824 <9mariam7@gmail.com>
2024-09-11 20:04:49 +0300

    Update shop_luma.py
    добавление таймаутов, пока не работает

--- homework/morera_marina/test_UI_unique_name/pages/shop_luma.py
+++ homework/morera_marina/test_UI_unique_name/pages/shop_luma.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import Page
+from time import sleep
 
 
 class ShopLuma:
@@ -14,27 +15,23 @@ class ShopLuma:
         yes = self.page.locator('//div[@data-role="title" and text()="New"]/following-sibling::div[@data-role="content"]//a[contains(text(), "Yes")]')
         yes.click()
         check_count = self.page.locator('.product-image-container').count()
-        total = self.page.locator('xpath=//p[@id="toolbar-amount"]/span[@class="toolbar-number"]').inner_text()
+        total = self.page.locator('xpath=//p[@id="toolbar-amount"]/span[@class="toolbar-number"]').nth(1).inner_text()
         number = int(total)
 
         assert check_count == number
 
     def add_item_to_compare(self):
-        item = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'product-image-wrapper'))
-        )
-        actions = ActionChains(self.driver)
-        actions.move_to_element(item).perform()
-        compare = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'tocompare'))
-        )
-        actions.click(compare).perform()
-        item_text = self.driver.find_element(By.CLASS_NAME, 'product-item-link').text
-        self.driver.execute_script("window.scrollTo(0, 500)")
-        compare_text = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="compare-items"]/li/strong/a'))
-        )
-        assert item_text == compare_text.text
+        item = self.page.locator('.product-image-wrapper').nth(0)
+        item.hover()
+        compare = self.page.locator('.tocompare').nth(0)
+        compare.wait_for(state='visible', timeout=60000)
+        compare.click()
+        item_text = self.page.locator('.product-item-link').nth(0).inner_text()
+
+        compare_text = self.page.locator('//*[@id="compare-items"]/li/strong/a').inner_text()
+        print('text', compare_text, 'text 2', item_text)
+        sleep(5)
+        assert item_text == compare_text
 
     def add_item_to_wishlist(self):
         item = WebDriverWait(self.driver, 10).until(
