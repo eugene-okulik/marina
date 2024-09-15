20f3b510a46b7c6bd163f0662cf98b2eaba62efc
Marina1824 <9mariam7@gmail.com>
2024-09-15 22:46:24 +0300

    shop luma compare

--- homework/morera_marina/test_UI_unique_name/pages/shop_luma.py
+++ homework/morera_marina/test_UI_unique_name/pages/shop_luma.py
@@ -27,23 +27,18 @@ class ShopLuma:
         compare.wait_for(state='visible', timeout=60000)
         compare.click()
         item_text = self.page.locator('.product-item-link').nth(0).inner_text()
-
-        compare_text = self.page.locator('//*[@id="compare-items"]/li/strong/a').inner_text()
-        print('text', compare_text, 'text 2', item_text)
-        sleep(5)
+        compare_link = self.page.locator('//ol[@id="compare-items"]//li//strong/a')
+        compare_link.wait_for(state='visible', timeout=60000)
+        compare_text = compare_link.inner_text()
         assert item_text == compare_text
 
     def add_item_to_wishlist(self):
-        item = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'product-image-wrapper'))
-        )
+        item = self.page.locator('.product-image-wrapper')
         actions = ActionChains(self.driver)
         actions.move_to_element(item).perform()
-        wishlist = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'towishlist'))
-        )
+        wishlist =  self.page.locator('.towishlist')
         actions.click(wishlist).perform()
 
     def check_wish_list_message_is(self, expected_text):
-        item_text = self.driver.find_element(By.CLASS_NAME, 'messages').text
+        item_text = self.page.locator('.messages').inner_text()
         assert item_text == expected_text
