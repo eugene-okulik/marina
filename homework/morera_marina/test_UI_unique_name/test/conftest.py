acf7ad76c9d4ad550ddd7e0b16f834b0ce55036b
Marina1824 <9mariam7@gmail.com>
2024-11-30 23:45:59 +0200

    Update conftest.py

--- homework/morera_marina/test_UI_unique_name/test/conftest.py
+++ homework/morera_marina/test_UI_unique_name/test/conftest.py
@@ -1,7 +1,29 @@
 import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from homework.morera_marina.test_UI_unique_name.pages.shop_luma import ShopLuma
 from homework.morera_marina.test_UI_unique_name.pages.sales import Sales
 from homework.morera_marina.test_UI_unique_name.pages.customer_login import Customerlogin
+from playwright.sync_api import BrowserContext
+from time import sleep
+
+
+@pytest.fixture()
+def driver():
+    options = Options()
+    options.add_argument('--headless')
+    chrome_driver = webdriver.Chrome(options=options)
+    sleep(3)
+    chrome_driver.implicitly_wait(5)
+    return chrome_driver
+
+
+@pytest.fixture()
+def page(context: BrowserContext, playwright):
+    playwright.selectors.set_test_id_attribute("id")
+    page = context.new_page()
+    page.set_viewport_size({'width': 1920, 'height': 1080})
+    return page
 
 
 @pytest.fixture()
