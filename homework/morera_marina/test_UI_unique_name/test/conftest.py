e31e815048dc8fc9ccc5ce1caaa742f6759e6494
Marina1824 <9mariam7@gmail.com>
2024-11-30 23:52:16 +0200

    Update conftest.py

--- homework/morera_marina/test_UI_unique_name/test/conftest.py
+++ homework/morera_marina/test_UI_unique_name/test/conftest.py
@@ -4,12 +4,12 @@ from selenium.webdriver.chrome.options import Options
 from homework.morera_marina.test_UI_unique_name.pages.shop_luma import ShopLuma
 from homework.morera_marina.test_UI_unique_name.pages.sales import Sales
 from homework.morera_marina.test_UI_unique_name.pages.customer_login import Customerlogin
-from playwright.sync_api import BrowserContext
+from playwright.sync_api import sync_playwright
 from time import sleep
 
 
 @pytest.fixture()
-def driver():
+def selenium_driver():
     options = Options()
     options.add_argument('--headless')
     chrome_driver = webdriver.Chrome(options=options)
@@ -19,23 +19,26 @@ def driver():
 
 
 @pytest.fixture()
-def page(context: BrowserContext, playwright):
-    playwright.selectors.set_test_id_attribute("id")
-    page = context.new_page()
-    page.set_viewport_size({'width': 1920, 'height': 1080})
-    return page
+def playwright_context_and_page():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=True)
+        context = browser.new_context()
+        page = context.new_page()
+        page.set_viewport_size({'width': 1920, 'height': 1080})
+        yield page
+        browser.close()
 
 
 @pytest.fixture()
-def shop_luma(page):
-    return ShopLuma(page)
+def shop_luma(playwright_context_and_page):
+    return ShopLuma(playwright_context_and_page)
 
 
 @pytest.fixture()
-def sales(page):
-    return Sales(page)
+def sales(playwright_context_and_page):
+    return Sales(playwright_context_and_page)
 
 
 @pytest.fixture()
-def login_page(page):
-    return Customerlogin(page)
+def login_page(playwright_context_and_page):
+    return Customerlogin(playwright_context_and_page)
