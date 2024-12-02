0b60b0c0587d47f62c2a4c61e1f8fa5fdd6085bd
Marina1824 <9mariam7@gmail.com>
2024-12-02 23:42:18 +0200

    Update conftest.py

--- homework/morera_marina/test_UI_unique_name/test/conftest.py
+++ homework/morera_marina/test_UI_unique_name/test/conftest.py
@@ -21,16 +21,11 @@ def selenium_driver():
 @pytest.fixture()
 def playwright_context_and_page():
     with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
+        browser = playwright.chromium.launch(headless=True)
         context = browser.new_context()
         page = context.new_page()
-        #page.set_viewport_size({'width': 1920, 'height': 1080})
         yield page
         browser.close()
-
-
-
-
 
 
 @pytest.fixture()
